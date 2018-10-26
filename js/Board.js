a794b393b38d68d325f637d63202c45986b9e687
zalo Riera <nadalriera84@gmail.com>
2018-10-26 02:13:34 +0200

    wip collision harpoon-ball

--- js/Board.js
+++ js/Board.js
@@ -1,10 +1,4 @@
-
-var canvas = document.getElementById("portrait");
-canvas.width = window.innerWidth -10;
-canvas.height = window.innerHeight -10;
-var ctx = canvas.getContext("2d");
-
-function Board(){
+ function Board(){
     this.x = 0;
     this.y = 0;
     this.width = canvas.width;
@@ -13,9 +7,70 @@ function Board(){
    
     this.score = 0;
 }
+Board.prototype.start = function() {
+    AnimationLoop();
+}
+Board.prototype.gameOver = function(){
+    console.log("game over");
+} 
 
 Board.prototype.drawBoard = function() {
     this.img.src = "imgs/background1.png";
     ctx.drawImage(this.img,0, 0, canvas.width, canvas.height);
     
 }
+
+Board.prototype.getDistance = function(x1, y1, x2, y2) {
+    var xDistance = x2 -x1;
+    var yDistanceY = y2 -y1;
+    return Math.sqrt(Math.pow(xDistance, 2) +Math.pow(yDistanceY, 2));
+}
+
+Board.prototype.collisionBallplayer = function(){
+    var distance = this.getDistance(player.x +player.width/2, player.y +player.height/2, balloon.x, balloon.y);
+   // console.log("distancia entre pares " + distance)
+    if( distance < balloon.radius + player.width/2){
+        this.lives -=1;
+        console.log("one live less lives");
+        balloon.dy = -balloon.dy;
+       if(this.lives <= 0){
+           console.log("game over");
+           this.gameOver();
+       }
+    }
+}
+ Board.prototype.addNewHarpoon = function(e) {
+    if(e.keyCode === 96){
+        var harpoon = new Harpoon(player.x, player.y)
+        harpoons.push(harpoon);
+    }
+ }
+
+
+
+  function AnimationLoop() {
+    board.drawBoard();
+    player.drawPlayer();
+    board.collisionBallplayer();
+    for(var i=0 ; i< harpoons.length; i++){
+        harpoons[i].drawHarpoon();
+        harpoons[i].updateHarpoon();
+    }
+    
+   
+    balloon.draw();
+    balloon.updateBall();
+   
+   requestAnimationFrame(AnimationLoop);
+    
+}
+
+
+
+
+
+
+
+
+
+  
--- js/Weapons.js
+++ js/Weapons.js
@@ -1,50 +1,23 @@
-/*var harpoonsArray = [];
-
- function Harpoon(x ,y, speed) {
+  function Harpoon(x ,y) {
      this.x = x;
      this.y = y;
-     this.speed = speed;
+     this.speed = 5;
+     this.width = 15;
+     this.height = 15;
  }
  
    Harpoon.prototype.drawHarpoon = function() {
-          ctx.beginpath();
+          ctx.beginPath();
           ctx.fillStyle = "rgba(44, 133, 240)";
           ctx.fillRect(this.x, this.y, 20,20);
           ctx.stroke();
-          this.y += 3.5;
-          
+        
       }
     
     
     Harpoon.prototype.updateHarpoon = function(){
-        if(this.y > canvas.height){
-            this.y = ctx.clearRect(this.c, this.y, 0,0);
-        }
-
+        this.y -= this.speed;
     }
-
-    function animateHarpoon() {
-        requestAnimationFrame(animateHarpoon);
-        ctx.beginpath();
-          ctx.fillStyle = "rgba(44, 133, 240)";
-          ctx.fillRect(this.x, this.y, 20,20);
-          ctx.stroke();
-
-          this.y += 3.5; 
-    }
- 
-
- Harpoon.prototype.harpoonFire = function() {
-     if (playerpositionX[i] === this.x){
-         drawHarpoon();
-
-     }
-
- }
-
- Harpoon.prototype.harpoonCollisionBall = function(){
-
- }*/
 
  
 
